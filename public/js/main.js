519bcc054389d97b0eb8bb574645eee6dc56c8f4
Marllon Freitas <marllonfreitas64@gmail.com>
2021-02-21 23:02:24 -0300

    adding dark mode to the project
    adding dark mode and saving in on local storage

--- public/js/main.js
+++ public/js/main.js
@@ -11,6 +11,49 @@ const Modal = {
     }
 }
 
+/**dark mode */
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('darkMode'); 
+
+const darkModeToggle = document.querySelector("input[name=theme]")
+
+const enableDarkMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('darkmode');
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('darkMode', 'enabled');
+  
+}
+
+const disableDarkMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('darkmode');
+  // 2. Update darkMode in localStorage 
+  localStorage.setItem('darkMode', null);
+}
+ 
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+  // get their darkMode setting
+  darkMode = localStorage.getItem('darkMode'); 
+  
+  // if it not current enabled, enable it
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+  // if it has been enabled, turn it off  
+  } else {  
+    disableDarkMode(); 
+  }
+});
+
+
+
 const Storage = {
     get() {
         return JSON.parse(localStorage.getItem("My.finances:transactions")) || []
